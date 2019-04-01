b64802b2e23c4082692e993805d01eadb679e55a
active-object-6703 <nerajno@gmail.com>
2019-04-01 12:54:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
      puts "#{season.capitalize}:"
     holiday.each do |day, supplies|
         # need notes on the .map method &&
-        puts "  #{day.to_s.split("_").map.titleize.join(" ")}: #{supplies.join(", ")}"
+        puts "  #{day.to_s.split("_").map(&:capitalize).join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
